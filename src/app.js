22637d5382981f771bed43689abb58063e143255
anarehder <anapaula.rehder@gmail.com>
2023-04-16 10:55:00 -0300

    Get Participants

--- src/app.js
+++ src/app.js
@@ -12,7 +12,6 @@ app.use(cors());
 app.use(express.json());
 dotenv.config();
 
-
 const mongoClient = new MongoClient(process.env.DATABASE_URL)
 try {
     await mongoClient.connect()
@@ -49,19 +48,52 @@ app.post("/participants", async (req, res) => {
             type: 'status', time: dayjs().format('HH:mm:ss') };
         await db.collection("participants").insertOne(newUser);
         await db.collection("messages").insertOne(newMessage);
-        console.log(newMessage);
         res.sendStatus(201);
     } catch (err) {
         res.status(500).send(err.message)
     }
 });
 
-app.get("/participants", (req, res) => {
-    res.send("OK");
-});
+app.get("/participants", async (req, res) => {
+    try {
+        const listaParticipantes = await db.collection("participants").find().toArray()
+        console.log(listaParticipantes)
+        res.send(listaParticipantes)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+})
 
-app.post("/messages", (req, res) => {
-    res.send("OK");
+app.post("/messages",async (req, res) => {
+    const { to, text, type } = req.body;
+    const from = req.headers.user;
+
+    const messageSchema = joi.object({
+        to:joi.string().min(1).required(),
+        text: joi.string().min(1).required(),
+        type: joi.string().min(1).valid('message', 'private_message').required()
+    })
+
+    const validation = messageSchema.validate(req.body, { abortEarly: false });
+
+    if (validation.error) {
+        const errors = validation.error.details.map(detail => detail.message)
+        return res.status(422).send(errors)
+    }
+
+    try {
+        const user = await db.collection("participants").findOne({ name: from });
+        if (user) {
+            const newMessage = {from, to, text, type, time: dayjs().format('HH:mm:ss')};
+            await db.collection("messages").insertOne(newMessage);
+            return res.sendStatus(201);
+        }
+        else {
+            return res.sendStatus(422)
+        }
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
 });
 
 app.get("/messages", (req, res) => {

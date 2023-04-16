b7485cb3ab6dfa958baa57110988af03a4f05856
anarehder <anapaula.rehder@gmail.com>
2023-04-16 10:21:22 -0300

    Post Participantes

--- src/app.js
+++ src/app.js
@@ -1,17 +1,59 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
+import { MongoClient, ObjectId } from "mongodb";
+import dotenv from "dotenv";
+import joi from "joi";
+import dayjs from "dayjs";
 
 const app = express();
 
 // Configs
 app.use(cors());
 app.use(express.json());
+dotenv.config();
 
-DATABASE_URL = "";
 
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+try {
+    await mongoClient.connect()
+    console.log("MongoDB conectado!")
+} catch (err) {
+    console.log(err.message)
+}
+const db = mongoClient.db()
 
-app.post("/participants", (req, res) => {
-    res.send("OK");
+//PARTICIPANTE
+// {name: 'João', lastStatus: 12313123} // 
+//MENSAGEM
+//{from: 'João', to: 'Todos', text: 'oi galera', type: 'message', time: '20:04:37'}
+
+app.post("/participants", async (req, res) => {
+    const { name } = req.body;
+
+    const userSchema = joi.object({
+        name: joi.string().required(),
+    })
+    const validation = userSchema.validate(req.body, { abortEarly: false });
+
+    if (validation.error) {
+        const errors = validation.error.details.map(detail => detail.message)
+        return res.status(422).send(errors)
+    }
+
+    try {
+        const user = await db.collection("participants").findOne({ name: name })
+        if (user) return res.status(409).send("Esse usuário já existe!")
+
+        const newUser = {name: name, lastStatus: Date.now()};
+        const newMessage = { from: name, to: "Todos", text: "entra na sala...", 
+            type: 'status', time: dayjs().format('HH:mm:ss') };
+        await db.collection("participants").insertOne(newUser);
+        await db.collection("messages").insertOne(newMessage);
+        console.log(newMessage);
+        res.sendStatus(201);
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
 });
 
 app.get("/participants", (req, res) => {
